3a122d2106b91578e89b8464f959e903c162cffe
RonakBagdai <ronakbagdai332@gmail.com>
2025-07-16 23:52:59 +0530

    Update Vite configuration to set base path to root for proper rendering

--- mern-graphql-breach-checker/client/vite.config.js
+++ mern-graphql-breach-checker/client/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/sheryians-project-exercise/",
   plugins: [react()],
+  base: "/", // <-- THIS IS IMPORTANT FOR RENDER!
 });
